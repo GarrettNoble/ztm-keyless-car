569a9a13e116927c6f66f28a1ceb70e79d378154
Nyx <noble.garrettlee@gmail.com>
2020-03-21 21:24:19 -0500

    added script

---
+++ main.js
@@ -0,0 +1,12 @@
+var checkDriverAge2 = function () {
+    var age = prompt("How old are you?");
+    if (Number(age) < 18) {
+        alert("Sorry, you are too young to drive this car. Powering off!");
+    } else if (Number(age) > 18) {
+        alert("Powering on. Enjoy the ride!");
+    } else if (Number(age) === 18) {
+        alert("Congratulations on your first year of driving. Enjoy the ride!");
+    }
+}
+
+checkDriverAge2();
