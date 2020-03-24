ecae4864780ccf576a0fc50296ccec3464943c11
Nyx <noble.garrettlee@gmail.com>
2020-03-23 23:52:25 -0500

    updated script

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
-var checkDriverAge2 = function () {
-    var age = prompt("How old are you?");
-    if (Number(age) < 18) {
-        alert("Sorry, you are too young to drive this car. Powering off!");
-    } else if (Number(age) > 18) {
-        alert("Powering on. Enjoy the ride!");
-    } else if (Number(age) === 18) {
-        alert("Congratulations on your first year of driving. Enjoy the ride!");
-    }
-}
+// function checkDriverAge() {
+//   var age = prompt("How old are you?");
 
-checkDriverAge2();
+//   if (Number(age) < 18) {
+//     alert("You are too young to be driving this car. Powering off.");
+//   } else if (Number(age) > 18) {
+//     alert("Powering on. Enjoy the drive!");
+//   } else if (Number(age) === 18) {
+//     alert("Congratulations on your first year of driving. Enjoy the ride!");
+//   }
+// }
+
+// checkDriverAge();
+
+// var checkDriverAge2 = function() {
+//   var age = prompt("How old are you?");
+
+//   if (Number(age) < 18) {
+//     alert("You are too young to be driving this car. Powering off.");
+//   } else if (Number(age) > 18) {
+//     alert("Powering on. Enjoy the drive!");
+//   } else if (Number(age) === 18) {
+//     alert("Congratulations on your first year of driving. Enjoy the ride!");
+//   }
+// };
+
+// checkDriverAge2();
